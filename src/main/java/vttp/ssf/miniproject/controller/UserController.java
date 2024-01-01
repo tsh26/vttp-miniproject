57c42813452edac8c103cc91909e3fc43a3c611f
Natasha <sn.natasha26@gmail.com>
2024-01-01 20:21:52 +0800

    added buttons to navigate through pages

--- src/main/java/vttp/ssf/miniproject/controller/UserController.java
+++ src/main/java/vttp/ssf/miniproject/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,6 +50,40 @@ public class UserController {
         }
 
         sess.setAttribute("user", user);
+        return mavValid;
+    }
+
+    @GetMapping("/guest")
+    public ModelAndView getGuestHomepage() {
+        ModelAndView mav = new ModelAndView("guest");
+        mav.addObject("code", busApiSvc.getBusStopCode());
+        mav.addObject("busStop", new BusStop());
+
+        return mav;
+    }
+
+    @PostMapping("/guestResult")
+    public ModelAndView searchGuestResult(@Valid BusStop busStop, BindingResult bindingResult) {
+        ModelAndView mav = new ModelAndView("guest");
+        ModelAndView mavValid = new ModelAndView("guestResult");
+        mav.addObject("busStop", busStop);
+
+        if (bindingResult.hasErrors()) {
+            return mav;
+        }
+
+        // Fetch bus information based on the bus stop code
+        List<BusArrival> busInfo = busApiSvc.getBusStopInfo(Integer.parseInt(busStop.getBusStopCode()));
+        mavValid.addObject("busInfo", busInfo);
+
+        // Fetch additional information and add it to the ModelAndView
+        BusStop additionalInfo = busApiSvc.fetchAdditionalInfo(busStop.getBusStopCode());
+        if (additionalInfo != null) {
+            busStop.setRoadName(additionalInfo.getRoadName());
+            busStop.setDescription(additionalInfo.getDescription());
+            mavValid.addObject("busStop", busStop);
+        }
+
         return mavValid;
     }
 
@@ -104,13 +139,24 @@ public class UserController {
             mavValid.addObject("busStop", busStop);
         }
 
-        // Get the user from the session 
+        // Get the user from the session
         User user = (User) sess.getAttribute("user");
 
         busUserSvc.addBusStop(user.getUsername(), busStop);
         mavValid.addObject("codes", busApiSvc.getBusStopCode());
         mavValid.addObject("busStopBookmark", busUserSvc.getAllBusStops(user.getUsername()));
         return mavValid;
+    }
+
+    @PostMapping("/retrieveBookmarks")
+    public ModelAndView retrieveBookmarks(@ModelAttribute("busStop") BusStop busStop, HttpSession sess) {
+        ModelAndView mav = new ModelAndView("bookmark");
+
+        User user = (User) sess.getAttribute("user");
+        mav.addObject("codes", busApiSvc.getBusStopCode());
+        mav.addObject("busStopBookmark", busUserSvc.getAllBusStops(user.getUsername()));
+
+        return mav;
     }
 
     @PostMapping("/delete")
