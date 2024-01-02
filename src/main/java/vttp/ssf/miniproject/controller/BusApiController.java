b6f5a167d8fc941bfcae831a8d44af6831613ace
Natasha <sn.natasha26@gmail.com>
2024-01-02 20:00:15 +0800

    added api for bookmarks

--- src/main/java/vttp/ssf/miniproject/controller/BusApiController.java
+++ src/main/java/vttp/ssf/miniproject/controller/BusApiController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vttp.ssf.miniproject.model.BusArrival;
+import vttp.ssf.miniproject.model.BusStop;
 import vttp.ssf.miniproject.service.BusApiService;
+import vttp.ssf.miniproject.service.BusUserService;
 
 @RestController
 @RequestMapping("/api")
@@ -18,9 +20,21 @@ public class BusApiController {
     @Autowired
     private BusApiService busApiSvc;
 
+    @Autowired
+    private BusUserService busUserSvc;
+
+    // Fetch Bus Stop information in real-time
     @GetMapping("/bus-info/{busStopCode}")
     public List<BusArrival> getBusInfo(@PathVariable int busStopCode) {
         // Fetch and return bus information as a REST response
         return busApiSvc.getBusStopInfo(busStopCode);
     }
+
+    // Fetch user's bookmarks
+    @GetMapping("/user/{username}/bookmarks")
+    public List<BusStop> getUserBookmarks(@PathVariable String username) {
+
+        return busUserSvc.getAllBusStops(username);
+    }
+
 }
